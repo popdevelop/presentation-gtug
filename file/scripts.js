78380cf99f0c16550ab0b0caa98983d04665ec1f
Sebastian Wallin <sebastian.wallin@gmail.com>
2011-04-12 15:48:16 +0200

    static presentation

--- file/scripts.js
+++ file/scripts.js
@@ -1,9 +1,51 @@
 document.writeln('<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>');
+document.writeln('<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js"></script>');
 
 $(document).ready(function() {
-    setTimeout(function() {
-        Cufon.replace('h1');
-        Cufon.replace('h2');
-        Cufon.replace('h3');
-    }, 400);
+  setTimeout(function() {
+    Cufon.replace('h1');
+    Cufon.replace('h2');
+    Cufon.replace('h3');
+  }, 400);
 });
+
+
+$(function() {
+  $('div.content').live('showoff:show', function(evt) {
+    var bg_img = $('img[alt=background]', evt.target);
+    if (bg_img.size() > 0) {
+      var src = bg_img.attr('src');
+      bg_img.hide();
+      
+      // Set new background on body
+      $('body')
+        .css('background', 'url(' + src + ') no-repeat center center fixed')
+        .css('-webkit-background-size', 'cover')
+        .css('-moz-background-size', 'cover')
+        .css('-o-background-size', 'cover')
+        .css('background-size', 'cover');
+    } else {
+      $('body').css('background-image', '');
+    }
+  });
+});
+
+var Gmap = {
+  LatLng: function() {
+    return new google.maps.LatLng(55.588047, 13.000946);
+  },
+  Options: function() {
+    var latlng = this.LatLng();
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    return myOptions;
+  },
+  OptionsUI: function() {
+    var opts = this.Options();
+    opts.disableDefaultUI = true;
+    return opts;
+  }
+};
