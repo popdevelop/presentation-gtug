04dd70f2b50b72ed36a492a530117bad27881518
Sebastian Wallin <sebastian.wallin@gmail.com>
2011-04-14 08:58:51 +0200

    static presentation

--- file/scripts.js
+++ file/scripts.js
@@ -1,6 +1,19 @@
 document.writeln('<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>');
 document.writeln('<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js"></script>');
 
+/* Google analytics */
+document.writeln(" \
+<script type=\"text/javascript\"> \
+  var _gaq = _gaq || []; \
+  _gaq.push(['_setAccount', 'UA-7232273-11']); \
+  _gaq.push(['_trackPageview']); \
+  (function() { \
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; \
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; \
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); \
+  })(); \
+</script>");
+
 /* XXX: This is ugly */
 $(document).ready(function() {
   setTimeout(function() {
@@ -11,7 +24,6 @@ $(document).ready(function() {
 
     /* Open all links in a new window */
     $("a[href^=http]").each(function(){
-       console.log('a')
        if(this.href.indexOf(location.hostname) == -1) {
           $(this).attr({
              target: "_blank",
@@ -26,22 +38,17 @@ $(document).ready(function() {
 $(function() {
   $('div.content').live('showoff:show', function(evt) {
     var bg_img = $('img[alt=background]', evt.target);
+    var old_bg = '';
     if (bg_img.size() > 0) {
       var src = bg_img.attr('src');
       bg_img.hide();
-      
       // Set new background on body
+      old_bg = $('body').css('background-image');
       $('body')
         .css('background-image', 'url(' + src + ')')
-        .css('background-repeat', 'no-repeat')
-        .css('background-position', 'center center')
-        .css('background-attachment', 'fixed')
-        .css('-webkit-background-size', 'cover')
-        .css('-moz-background-size', 'cover')
-        .css('-o-background-size', 'cover')
-        .css('background-size', 'cover');
+        .addClass('fullScreen');
     } else {
-      $('body').css('background-image', '');
+      $('body').css('background-image', old_bg).removeClass('fullScreen');
     }
   });
 });
